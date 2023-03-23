8b002ffd6290fda397839fc9011cc4e49b127704
Rik Hennekens <r.hennekens@student.fontys.nl>
2023-03-23 11:19:28 +0100

    db connection

--- main.go
+++ main.go
@@ -2,14 +2,43 @@ package main
 
 import (
 	"crypto/rand"
+	"database/sql"
 	"fmt"
 	"log"
 	"math/big"
 )
 
+const (
+	dbname = "mijndb"
+	dbuser = "rik"
+	dbpass = "SQLR1k"
+)
+
+var database *sql.DB //database connection
+
 func main() {
+	//Connect to the database
+	db, err := sql.Open("postgres", "dbname="+dbname+" user="+dbuser+" password="+dbpass+" sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	database = db
+	if err := createTable(); err != nil {
+		log.Fatal(err)
+	}
+
 	generatePassword(10, true, true)
 
+}
+
+func createTable() error {
+	query := `CREATE TABLE IF NOT EXISTS passwords (
+		id			SERIAL	PRIMARY KEY,
+		password	TEXT	NOT NULL
+	);`
+	_, err := database.Exec(query)
+	return err
 }
 
 func generatePassword(length int, includeNumbers bool, includeSymbols bool) string {
